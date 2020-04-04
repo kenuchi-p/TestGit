69349801b304047c9d7480789113d45b8db9ff87
内山堅司 <kenuchi8_8@awave.sakura.ne.jp>
2020-04-04 11:26:15 +0900

    コミットのテスト

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TestGit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.メインMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuShowMessageBox = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.メインMToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // メインMToolStripMenuItem
+            // 
+            this.メインMToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuShowMessageBox});
+            this.メインMToolStripMenuItem.Name = "メインMToolStripMenuItem";
+            this.メインMToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.メインMToolStripMenuItem.Text = "メイン(&M)";
+            // 
+            // mnuShowMessageBox
+            // 
+            this.mnuShowMessageBox.Name = "mnuShowMessageBox";
+            this.mnuShowMessageBox.Size = new System.Drawing.Size(253, 26);
+            this.mnuShowMessageBox.Text = "メッセージボックスの表示(&B)";
+            this.mnuShowMessageBox.Click += new System.EventHandler(this.mnuShowMessageBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem メインMToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuShowMessageBox;
+    }
+}
+
